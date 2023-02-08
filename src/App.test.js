f5033af9d31cde064272ce3f55b3423aff253ee4
jwong65 <jason.wong6507@gmail.com>
2023-02-07 19:06:15 -0500

    Added react-test-renderer, imported the Jest test

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import renderer from 'react-test-renderer'
 
 test('renders learn react link', () => {
   render(<App />);
