2033316d0f7bb274e4d7bedb31dc159449f2f1a5
jwong65 <jason.wong6507@gmail.com>
2023-02-07 19:34:51 -0500

    Fixed toJSON()

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import GitHubCard from './components/GitHubCard';
 
 function App() {
   return (
--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import App from './App';
 import renderer from 'react-test-renderer'
 
 test('renders a snapshot', () => {
-  const tree = renderer.create(<App/>).toJSON
+  const tree = renderer.create(<App/>).toJSON()
   expect(tree).toMatchSnapshot()
 });
 
