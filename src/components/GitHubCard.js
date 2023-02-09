680aa489a63455233fc7b650deb03d08c9353c43
jwong65 <jason.wong6507@gmail.com>
2023-02-09 18:02:07 -0500

    Added text on card component

--- src/components/GitHubCard.js
+++ src/components/GitHubCard.js
@@ -3,6 +3,11 @@ import Card from 'react-bootstrap/Card'
 
 export default function GitHubCard() {
   return (
-    <Card></Card>
+    <Card style={{width:'18rem'}}>
+        <Card.Body>
+            <Card.Title>Title of GitHub Card</Card.Title>
+            <Card.Text>Test of text on card</Card.Text>
+        </Card.Body>
+    </Card>
   )
 }
