68544f7eae8b3beaccd63808b3461ec52631dd80
jwong65 <jason.wong6507@gmail.com>
2023-02-09 18:07:46 -0500

    Added link and removed image placeholder for testing

--- src/components/GitHubCard.js
+++ src/components/GitHubCard.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
 
+
 export default function GitHubCard() {
   return (
     <Card style={{width:'18rem'}}>
         <Card.Body>
             <Card.Title>Title of GitHub Card</Card.Title>
             <Card.Text>Test of text on card</Card.Text>
+            <Card.Link href='#'>Link test</Card.Link>
+
+
         </Card.Body>
     </Card>
   )
